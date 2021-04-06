17c9853014fe38bb80b28baa3ff62dd08153fd69
Leon Jin <leon.jyq@outlook.com>
2021-04-07 07:51:22 +0800

    fix input arguments

--- process.go
+++ process.go
@@ -2,25 +2,53 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
-	"github.com/360EntSecGroup-Skylar/excelize/v2"
 	"log"
 	"os"
+	"strings"
+
+	"github.com/360EntSecGroup-Skylar/excelize/v2"
 )
 
 func main() {
 
-	filepath := os.Args[1]
-	jsonstr := os.Args[2]
+	var inputFile, outputFile, distributorRule, d2cRule string
+	flag.StringVar(&inputFile, "in", "", "input file path")
+	flag.StringVar(&outputFile, "out", "", "output file path")
+	flag.StringVar(&distributorRule, "distributor", "", "rule for Distributor")
+	flag.StringVar(&d2cRule, "d2c", "", "rule for D2C")
 
-	fmt.Println(jsonstr)
+	flag.Parse()
+	fmt.Println(distributorRule)
+	fmt.Println(d2cRule)
+	fmt.Println(inputFile)
+	fmt.Println(outputFile)
 
-	f, err := excelize.OpenFile(filepath)
+	f, err := excelize.OpenFile(inputFile)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	sheets := f.GetSheetList()
+	for _, sheet := range sheets {
+		fmt.Println(sheet)
+		sheetType := strings.Split(sheet, "_")[1]
+		if strings.EqualFold(sheetType, "distributor") {
+			fmt.Println(sheetType)
+		} else if strings.EqualFold(sheetType, "d2c") {
+			fmt.Println(sheetType)
+		}
+
+		col, err := f.GetCols(sheet)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	return
 	rows, err := f.GetRows("MHKN_D2C")
 	if err != nil {
 		fmt.Println(err)
